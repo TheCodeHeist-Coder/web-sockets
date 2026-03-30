b99154d6fcffa26d939057137e91f1256296b4cd
Rajkumar <thecodeheist01@gmail.com>
2026-03-31 00:00:03 +0530

    backend_sockets_done

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import express from 'express';
+import http from 'http';
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+let userCount = 0;
+wss.on("connection", function connection(ws) {
+    ws.on('error', console.error);
+    ws.on("message", function message(data, isBinary) {
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+    console.log(`UserCount: ${++userCount}`);
+    ws.send("Welcome to the WebSocket server!");
+});
+server.listen(8080, () => {
+    console.log('Server is listening on port 8080');
+});
+//# sourceMappingURL=index.js.map
